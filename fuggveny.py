d10fcdb55f1a1996a4f9de6c9538ea8521f39b85
kisssd <kisssd70&gmail.com>
2026-08-20 13:46:50 +0200

    Add initial implementation of functions and main program

---
+++ fuggveny.py
@@ -0,0 +1,10 @@
+def fuggveny1():
+    print("Ez az első függvény!")
+
+def fuggveny2(uzenet):
+    print("Ez a második függvény, az üzenet: " + uzenet)
+
+def fuggveny3(szam1, szam2):
+    eredmeny = szam1 + szam2
+    print("Ez a harmadik függvény, az eredmény: " + str(eredmeny))
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+print("Sziasztok, ez egy Python program")
+
+print("Ez egy új sor!")
+print("Ez egy sor")
+print("Ez egy Python sor!")
+print("Írtam egy Python programot")
+print("Függvényt írtam")
